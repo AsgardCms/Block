58b1fc472456fcd59da08ac5f7cd5af868277053
Nicolas Widart <n.widart@gmail.com>
2015-04-20 11:11:09 +0200

    Add french translation

---
+++ Resources/lang/fr/blocks.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'title' => [
+        'blocks' => 'Blocks',
+        'create block' => 'Créer un block',
+        'edit block' => 'Editition d\'un block',
+    ],
+    'button' => [
+        'create block' => 'Créer un block',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+        'block created' => 'Block créer.',
+        'block not found' => 'Block non trouvé.',
+        'block updated' => 'Block mit à jour.',
+        'block deleted' => 'Block suprimer',
+    ],
+    'validation' => [
+        'body is required' => 'Le contenu est réquis',
+    ],
+    'online' => 'En ligne',
+    'name' => 'Nom',
+    'body' => 'Contenu',
+];
