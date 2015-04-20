7dc6ca1f94634ee6791b606c154daf1d18e19e7b
Nicolas Widart <n.widart@gmail.com>
2015-04-20 12:39:04 +0200

    Adding block form requests

---
+++ Http/Requests/UpdateBlockRequest.php
@@ -0,0 +1,37 @@
+<?php namespace Modules\Block\Http\Requests;
+
+use Modules\Core\Internationalisation\BaseFormRequest;
+
+class CreateBlockRequest extends BaseFormRequest
+{
+    public function translationRules()
+    {
+        return [
+            'body' => 'required',
+        ];
+    }
+
+    public function translationMessages()
+    {
+        return [
+            'body.required' => trans('block::blocks.validation.body is required'),
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+            'name' => 'required',
+        ];
+    }
+
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return [];
+    }
+}
