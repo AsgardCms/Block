81b8f61926b16b3588b632d41c346b3b83099f07
Nicolas Widart <n.widart@gmail.com>
2015-04-20 09:37:22 +0200

    Remove the blocks submenu

--- Composers/SidebarViewComposer.php
+++ Composers/SidebarViewComposer.php
@@ -10,21 +10,17 @@ class SidebarViewComposer extends BaseSidebarViewComposer
     public function compose(View $view)
     {
         $view->sidebar->group(trans('core::sidebar.content'), function (SidebarGroup $group) {
-            $group->addItem('Block', function (SidebarItem $item) {
-                $item->icon = 'fa fa-cube';
-                $item->weight = 10;
+            $group->addItem(trans('block::blocks.title.blocks'), function (SidebarItem $item) {
                 $item->authorize(
                     $this->auth->hasAccess('block.blocks.index')
                 );
-                $item->addItem(trans('block::blocks.title.blocks'), function (SidebarItem $item) {
-                    $item->icon = 'fa fa-cube';
-                    $item->weight = 0;
-                    $item->append('admin.block.block.create');
-                    $item->route('admin.block.block.index');
-                    $item->authorize(
-                        $this->auth->hasAccess('block.blocks.index')
-                    );
-                });
+                $item->icon = 'fa fa-cube';
+                $item->weight = 0;
+                $item->append('admin.block.block.create');
+                $item->route('admin.block.block.index');
+                $item->authorize(
+                    $this->auth->hasAccess('block.blocks.index')
+                );
             });
         });
     }
