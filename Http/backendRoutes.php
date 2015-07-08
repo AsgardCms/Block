6b491ac08f97668a47cfaec33e4ba31ec008546a
Nicolas Widart <n.widart@gmail.com>
2015-07-08 18:50:17 +0200

    Use singular parameter name. Use ::class notation.

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,14 +1,14 @@
 <?php
 
-$router->bind('blocks', function ($id) {
-    return app('Modules\Block\Repositories\BlockRepository')->find($id);
+$router->bind('block', function ($id) {
+    return app(\Modules\Block\Repositories\BlockRepository::class)->find($id);
 });
 
 $router->group(['prefix' =>'/block'], function () {
     get('blocks', ['as' => 'admin.block.block.index', 'uses' => 'BlockController@index']);
     get('blocks/create', ['as' => 'admin.block.block.create', 'uses' => 'BlockController@create']);
     post('blocks', ['as' => 'admin.block.block.store', 'uses' => 'BlockController@store']);
-    get('blocks/{blocks}/edit', ['as' => 'admin.block.block.edit', 'uses' => 'BlockController@edit']);
-    put('blocks/{blocks}', ['as' => 'admin.block.block.update', 'uses' => 'BlockController@update']);
-    delete('blocks/{blocks}', ['as' => 'admin.block.block.destroy', 'uses' => 'BlockController@destroy']);
+    get('blocks/{block}/edit', ['as' => 'admin.block.block.edit', 'uses' => 'BlockController@edit']);
+    put('blocks/{block}', ['as' => 'admin.block.block.update', 'uses' => 'BlockController@update']);
+    delete('blocks/{block}', ['as' => 'admin.block.block.destroy', 'uses' => 'BlockController@destroy']);
 });
