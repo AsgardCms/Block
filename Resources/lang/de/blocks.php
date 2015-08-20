9bad361864f4b74c6bca9d67c77aec7299094c8d
Nuno Ribeiro <nmpribeiro@gmail.com>
2015-08-20 15:39:58 +0000

    Added de locale pt and pt-BR

---
+++ Resources/lang/de/blocks.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    'title' => [
+        'blocks' => 'Blöcke',
+        'create block' => 'Blöcke erstellen',
+        'edit block' => 'Blöcke bearbeiten',
+    ],
+    'button' => [
+        'create block' => 'Blöcke erstellen',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+        'block created' => 'Blöcke erfolgreich erstellt.',
+        'block not found' => 'Blöcke nicht gefunden.',
+        'block updated' => 'Blöcke erfolgreich aktualisiert.',
+        'block deleted' => 'Blöcke erfolgreich gelöscht.',
+    ],
+    'validation' => [
+        'body is required' => 'Der Körper ist erforderlich',
+    ],
+    'online' => 'Online',
+    'name' => 'Namen',
+    'body' => 'Körper',
+];
