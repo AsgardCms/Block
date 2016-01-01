acd25011603543266d5b4b3031f3fbaa76cc3de2
Nicolas Widart <n.widart@gmail.com>
2016-01-01 14:21:58 +0100

    Using new Authorise middleware

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -3,11 +3,35 @@
 $router->bind('block', function ($id) {
     return app(\Modules\Block\Repositories\BlockRepository::class)->find($id);
 });
-$router->group(['prefix' =>'/block'], function () {
-    get('blocks', ['as' => 'admin.block.block.index', 'uses' => 'BlockController@index', 'middleware' => 'can:block.blocks.index']);
-    get('blocks/create', ['as' => 'admin.block.block.create', 'uses' => 'BlockController@create']);
-    post('blocks', ['as' => 'admin.block.block.store', 'uses' => 'BlockController@store']);
-    get('blocks/{block}/edit', ['as' => 'admin.block.block.edit', 'uses' => 'BlockController@edit']);
-    put('blocks/{block}', ['as' => 'admin.block.block.update', 'uses' => 'BlockController@update']);
-    delete('blocks/{block}', ['as' => 'admin.block.block.destroy', 'uses' => 'BlockController@destroy']);
+$router->group(['prefix' => '/block'], function () {
+    get('blocks', [
+        'as' => 'admin.block.block.index',
+        'uses' => 'BlockController@index',
+        'middleware' => 'can:block.blocks.index',
+    ]);
+    get('blocks/create', [
+        'as' => 'admin.block.block.create',
+        'uses' => 'BlockController@create',
+        'middleware' => 'can:block.blocks.create',
+    ]);
+    post('blocks', [
+        'as' => 'admin.block.block.store',
+        'uses' => 'BlockController@store',
+        'middleware' => 'can:block.blocks.store',
+    ]);
+    get('blocks/{block}/edit', [
+        'as' => 'admin.block.block.edit',
+        'uses' => 'BlockController@edit',
+        'middleware' => 'can:block.blocks.edit',
+    ]);
+    put('blocks/{block}', [
+        'as' => 'admin.block.block.update',
+        'uses' => 'BlockController@update',
+        'middleware' => 'can:block.blocks.update',
+    ]);
+    delete('blocks/{block}', [
+        'as' => 'admin.block.block.destroy',
+        'uses' => 'BlockController@destroy',
+        'middleware' => 'can:block.blocks.destroy',
+    ]);
 });
