d66c7fec2284550b4419a3415a3b5ad3d5c9aa26
Nicolas Widart <n.widart@gmail.com>
2016-06-08 21:44:41 +0200

    Using $router variable

--- Http/backendRoutes.php
+++ Http/backendRoutes.php
@@ -1,35 +1,39 @@
 <?php
 
+use Illuminate\Routing\Router;
+
+/** @var Router $router */
+
 $router->bind('block', function ($id) {
     return app(\Modules\Block\Repositories\BlockRepository::class)->find($id);
 });
-$router->group(['prefix' => '/block'], function () {
-    get('blocks', [
+$router->group(['prefix' => '/block'], function (Router $router) {
+    $router->get('blocks', [
         'as' => 'admin.block.block.index',
         'uses' => 'BlockController@index',
         'middleware' => 'can:block.blocks.index',
     ]);
-    get('blocks/create', [
+    $router->get('blocks/create', [
         'as' => 'admin.block.block.create',
         'uses' => 'BlockController@create',
         'middleware' => 'can:block.blocks.create',
     ]);
-    post('blocks', [
+    $router->post('blocks', [
         'as' => 'admin.block.block.store',
         'uses' => 'BlockController@store',
         'middleware' => 'can:block.blocks.store',
     ]);
-    get('blocks/{block}/edit', [
+    $router->get('blocks/{block}/edit', [
         'as' => 'admin.block.block.edit',
         'uses' => 'BlockController@edit',
         'middleware' => 'can:block.blocks.edit',
     ]);
-    put('blocks/{block}', [
+    $router->put('blocks/{block}', [
         'as' => 'admin.block.block.update',
         'uses' => 'BlockController@update',
         'middleware' => 'can:block.blocks.update',
     ]);
-    delete('blocks/{block}', [
+    $router->delete('blocks/{block}', [
         'as' => 'admin.block.block.destroy',
         'uses' => 'BlockController@destroy',
         'middleware' => 'can:block.blocks.destroy',
