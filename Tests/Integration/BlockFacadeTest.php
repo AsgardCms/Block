12b2b6e78541c0915a0d4ea8063e969421699b43
Nicolas Widart <n.widart@gmail.com>
2016-07-01 18:09:50 +0200

    Adding a block facade test

---
+++ Tests/Integration/BlockFacadeTest.php
@@ -0,0 +1,32 @@
+<?php namespace Modules\Block\Tests\Integration;
+
+use Illuminate\Database\Eloquent\Collection;
+
+class BlockFacadeTest extends BaseBlockTest
+{
+    /** @test */
+    public function it_links_global_facade_to_repository()
+    {
+        $this->block->create(['name' => 'testBlock', 'en' => ['body' => 'lorem en'], 'fr' => ['body' => 'lorem fr']]);
+
+        $this->assertInstanceOf(Collection::class, \Block::all());
+    }
+
+    /** @test */
+    public function it_works_as_repository()
+    {
+        $this->block->create([
+            'name' => 'test-block',
+            'en' => [
+                'body' => 'lorem en',
+                'online' => true,
+            ],
+            'fr' => [
+                'body' => 'lorem fr',
+                'online' => true,
+            ]
+        ]);
+
+        $this->assertEquals('lorem en', \Block::get('test-block'));
+    }
+}
