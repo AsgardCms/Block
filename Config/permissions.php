7612d574ec55268e252ac5c5666270a6c5dc2a26
Nicolas Widart <n.widart@gmail.com>
2016-07-06 21:53:45 +0200

    Adding nicer permission labels

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,9 +2,9 @@
 
 return [
     'block.blocks' => [
-        'index' => trans('core::core.list resource', ['name' => 'blocks']),
-        'create' => trans('core::core.create resource', ['name' => 'blocks']),
-        'edit' => trans('core::core.edit resource', ['name' => 'blocks']),
-        'destroy' => trans('core::core.destroy resource', ['name' => 'blocks']),
+        'index' => trans('block::blocks.list resource'),
+        'create' => trans('block::blocks.create resource'),
+        'edit' => trans('block::blocks.edit resource'),
+        'destroy' => trans('block::blocks.destroy resource'),
     ],
 ];
