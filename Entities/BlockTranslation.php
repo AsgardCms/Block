7b52f6b8688962f99102434ed652570f761d3478
Nicolas Widart <n.widart@gmail.com>
2017-07-27 16:39:37 +0200

    Adding new BlockContentIsRendering hook

--- Entities/BlockTranslation.php
+++ Entities/BlockTranslation.php
@@ -3,10 +3,18 @@
 namespace Modules\Block\Entities;
 
 use Illuminate\Database\Eloquent\Model;
+use Modules\Block\Events\BlockContentIsRendering;
 
 class BlockTranslation extends Model
 {
     public $timestamps = false;
     protected $fillable = ['online', 'body'];
     protected $table = 'block__blocks_translations';
+
+    public function getBodyAttribute($body)
+    {
+        event($event = new BlockContentIsRendering($body));
+
+        return $event->getBody();
+    }
 }
---
+++ Tests/Integration/BlockContentIsRenderingTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Modules\Block\Tests\Integration;
+
+use Illuminate\Support\Facades\Event;
+use Modules\Block\Events\BlockContentIsRendering;
+
+class BlockContentIsRenderingTest extends BaseBlockTest
+{
+    /** @test */
+    public function it_can_change_final_content()
+    {
+        Event::listen(BlockContentIsRendering::class, function (BlockContentIsRendering $event) {
+            $event->setBody('<strong>' . $event->getOriginal() . '</strong>');
+        });
+
+        $block = $this->block->create(['name' => 'testBlock', 'en' => ['body' => 'My Block Body'], 'fr' => ['body' => 'lorem fr']]);
+
+        $this->assertEquals('<strong>My Block Body</strong>', $block->body);
+    }
+}
