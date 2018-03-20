73316c9581ebc6524d836949e19538949a9e2536
Mariusz Fik <mariusz@fidano.pl>
2018-03-20 21:53:59 +0100

    Display backend translations. Add Polish lang.
    Signed-off-by: Mariusz Fik <mariusz@fidano.pl>

---
+++ Resources/lang/pl/blocks.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    'title' => [
+        'blocks' => 'Bloki',
+        'create block' => 'Tworzenie bloku',
+        'edit block' => 'Edycja bloku',
+    ],
+    'button' => [
+        'create block' => 'Utwórz blok',
+    ],
+    'table' => [
+    ],
+    'form' => [
+    ],
+    'messages' => [
+        'block created' => 'Blok pomyślnie utworzony.',
+        'block not found' => 'Blok nie znaleziony.',
+        'block updated' => 'Blok pomyślnie zaktualizowany.',
+        'block deleted' => 'Blok pomyślnie  usunięty.',
+    ],
+    'validation' => [
+        'body is required' => 'Zawartość bloku nie może być pusta',
+    ],
+    'online' => 'Online',
+    'name' => 'Nazwa',
+    'body' => 'Zawartość',
+    'list resource' => 'Wyświetlanie',
+    'create resource' => 'Tworzenie',
+    'edit resource' => 'Edycja',
+    'destroy resource' => 'Usuwanie',
+];
